a0e2990f6b66aaf76e0409018fc770ee96d19c8b
Brian Johnson <brian.l.johnson@gmail.com>
2022-12-01 07:57:12 -0600

    add comments on non-obvious code

--- day01/challenge2/src/main.rs
+++ day01/challenge2/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
                 total = total + num;
                 num
             }
-            Err(_) => {
+            Err(_) => { //if its not a number assume its a new line and we are on a new elf.  this is bad, but it works
                 println!("elf had total: {total}");
                 totals.push(total);
                 total = 0;
@@ -21,10 +21,10 @@ fn main() {
             }
         };
     }
-    totals.push(total);
+    totals.push(total); //add last elf, again bad
 
     totals.sort();
-    println!("total number of eolves {}",totals.len());
+    println!("total number of elves {}",totals.len());
     let mut sum = 0;
     for n in totals.len()-3..totals.len() {
         println!("elf {} has {} calories", n, totals[n]);
